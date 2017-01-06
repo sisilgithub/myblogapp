22a491cd6de40a97aa5ae445877398f12e4d0f70
sisil iyyakandy <sisilsasidharan@gmail.com>
2017-01-06 17:46:25 +0530

    commit welcome controller added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :users 
   root 'users#index'
+  get 'welcome/myaction'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
