eb8ce66cf12e1bd18d7a920f56853aabdb4bea06
sisil iyyakandy <sisilsasidharan@gmail.com>
2017-01-11 15:51:13 +0530

    password digest added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-	has_many :posts
-	validates :name, presence:true
-	validates :email, presence:true
+	has_many :posts	
+	before_save { self.email = email.downcase }
+	validates :name, presence:true, length: { maximum: 50 } 
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence:true,length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+	uniqueness: {casesensitive: false}
+	has_secure_password
+	validates :password, presence: true, length: { minimum: 6 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106141026) do
+ActiveRecord::Schema.define(version: 20170111082326) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "text",       limit: 255
@@ -21,10 +21,11 @@ ActiveRecord::Schema.define(version: 20170106141026) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
 
 end
