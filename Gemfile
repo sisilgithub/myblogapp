af84140685a3d1afaee3a6dbd8f8bc8f0e2333fd
sisil iyyakandy <sisilsasidharan@gmail.com>
2017-01-12 20:06:51 +0530

    use ssl and install puma at heroku

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
+gem 'puma',  '3.4.0'
 # Use mysql as the database for Active Record
 gem 'mysql2', '>= 0.3.13', '< 0.5'
 # Use SCSS for stylesheets
@@ -25,6 +26,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass', '3.3.6'
 gem 'bcrypt', '3.1.11'
+gem 'bootstrap_form'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,20 @@ class UsersController < ApplicationController
   # GET /users.json
   def index
     @users = User.all
+    @latest_posts= Post.all
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
     @user = User.find(params[:id])
+    @latest_posts= Post.all
   end
 
   # GET /users/new
   def new
     @user = User.new
+    @latest_posts= Post.all
   end
 
   # GET /users/1/edit
@@ -26,9 +29,10 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+    @latest_posts= Post.all
     respond_to do |format|
       if @user.save
+        flash[:success] = "Welcome to the Gadget Mania!"
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
