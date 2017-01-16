7203b5493a9467d24ab440575bd788fafa8c8cf3
sisil iyyakandy <sisilsasidharan@gmail.com>
2017-01-16 19:38:20 +0530

    login bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
     @latest_posts= Post.all
   end
 
@@ -70,7 +69,12 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      if logged_in? && current_user.id == params[:id].to_i
+
+        @user = User.find(params[:id])
+      else
+        redirect_to root_path
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
