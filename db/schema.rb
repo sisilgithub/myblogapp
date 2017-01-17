27911d58c3cd2c92b11f11f6ac998c31e4442435
sisil iyyakandy <sisilsasidharan@gmail.com>
2017-01-17 20:16:03 +0530

    add admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111135413) do
+ActiveRecord::Schema.define(version: 20170117143133) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "text",       limit: 255
@@ -24,9 +24,10 @@ ActiveRecord::Schema.define(version: 20170111135413) do
   create_table "users", force: :cascade do |t|
     t.string   "name",            limit: 255
     t.string   "email",           limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.string   "password_digest", limit: 255
+    t.boolean  "admin",                       default: false
   end
 
 end
